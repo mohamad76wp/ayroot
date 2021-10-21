2996a14f09630aa4821e8ccf6af163ef5b158cb3
mohamad76wp <mohamad76wp@gmail.com>
2021-10-21 13:14:17 +0330

    Modified Post, Category models

--- core/models.py
+++ core/models.py
@@ -1,12 +1,18 @@
+from typing import Tuple
 from django.db import models
 from django.db.models.deletion import SET_NULL
 from django.db.models.fields import TextField
 from django.utils.translation import ugettext_lazy as _
+from django.utils.text import slugify
 
-
-class category(models.Model):
-    title = models.CharField(_("Title"), max_length=32)
-    slug = models.SlugField(_("Slug"))
+# Category Section
+class Category(models.Model):
+    category_title = models.CharField(_("Title"), max_length=32, db_index=True)
+    slug = models.SlugField(
+        _("Slug"),
+        blank=True,
+        unique=True,
+    )
     parent = models.ForeignKey(
         "self",
         verbose_name=_("Parent"),
@@ -14,22 +20,56 @@ class category(models.Model):
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
+        default="self",
     )
-    create_at = models.DateTimeField(_("Create at"))
-    update_at = models.DateTimeField(_("Update at"))
+    create_at = models.DateTimeField(_("Create at"), auto_now_add=True)
+    update_at = models.DateTimeField(_("Update at"), auto_now=True)
+
+    class Meta:
+        verbose_name = _("Category")
+        verbose_name_plural = _("Categories")
+
+    def __str__(self):
+        return self.category_title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.category_title)
+        super(Category, self).save(*args, **kwargs)
 
 
-class post(models.Model):
+# Post Section
+class Post(models.Model):
     title = models.CharField(_("Title"), max_length=512, db_index=True)
-    slug = models.SlugField(_("Slug"), db_index=True, unique=True)
+    slug = models.SlugField(
+        _("Slug"),
+        blank=True,
+        unique=True,
+    )
     content = models.TextField(_("Post Content"))
-    category = models.ForeignKey(
-        "category",
+    category = models.ManyToManyField(
+        "Category",
         related_name="posts",
         verbose_name=_("Category"),
-        on_delete=models.SET_NULL,
-        null=True,
         blank=True,
     )
-    create_at = models.DateTimeField(_("Publish at"))
-    update_at = models.DateTimeField(_("Update at"))
+    create_at = models.DateTimeField(_("Publish at"), auto_now_add=True)
+    update_at = models.DateTimeField(_("Update at"), auto_now=True)
+
+    class Meta:
+        verbose_name = _("Post")
+        verbose_name_plural = _("Posts")
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
+
+    def display_category(self):
+        """Create a string for the Category. This is required to display Category in Admin."""
+        return ", ".join(
+            category.category_title for category in self.category.all()[:3]
+        )
+
+    display_category.short_description = "Category"
