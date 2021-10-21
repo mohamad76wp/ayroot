9685772b89a2fc135ce2f79227282fc829d9b8fb
mohamad76wp <mohamad76wp@gmail.com>
2021-10-21 13:11:22 +0330

    Register Post, Category ModelAdmin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Post, Category
 
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "display_category", "update_at", "create_at", "slug")
+    list_filter = ("category",)
+    readonly_fields = ("slug",)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("category_title", "parent", "slug")
+    readonly_fields = ("slug",)
+    list_filter = ("parent",)
